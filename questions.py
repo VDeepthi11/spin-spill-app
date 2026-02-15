5d84300612e52a87a15c7a577c0a50768ce104d3
vdeepthi11 <deepthiivellaboina@gmail.com>
2026-02-15 17:55:54 +0530

    Added questions.py and modularized questions

---
+++ questions.py
@@ -0,0 +1,34 @@
+questions = {
+
+    "😂 Funny": [
+        "What is your weirdest habit?",
+        "If animals could talk which would be rudest?",
+        "What’s the dumbest thing you believed as a kid?",
+        "If you were a ghost who would you haunt first?",
+        "What would you do if you woke up invisible?"
+    ],
+
+    "🤔 Deep": [
+        "What motivates you every day?",
+        "What is your biggest fear?",
+        "What makes you truly happy?",
+        "What is your dream life?",
+        "What does happiness mean to you?"
+    ],
+
+    "⚡ Rapid Fire": [
+        "Tea or Coffee?",
+        "Night or Morning?",
+        "City or Village?",
+        "Books or Movies?",
+        "Money or Love?"
+    ],
+
+    "😳 Embarrassing": [
+        "Who was your first crush?",
+        "What is your biggest secret?",
+        "Have you ever lied to your best friend?",
+        "What is your cringiest memory?",
+        "Have you ever waved back at someone who wasn’t waving at you?"
+    ]
+}
